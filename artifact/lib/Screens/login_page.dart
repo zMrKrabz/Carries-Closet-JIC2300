27a231da96ae9ec676f170473ecb7f22f97d919b
Steven Hua <stevenhua@gatech.edu>
2023-02-16 14:52:36 -0500

    creating new branch for hygiene form

--- artifact/lib/Screens/login_page.dart
+++ artifact/lib/Screens/login_page.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import "package:artifact/main.dart";
 
-
 import "package:artifact/home_page.dart";
 import "package:artifact/main.dart";
 
@@ -28,91 +27,81 @@ class _LoginPageState extends State<LoginPage> {
     double height = MediaQuery.of(context).size.height;
 
     return MaterialApp(
-      home: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: height * 1.0 / 18.0),
-              Stack(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SingleChildScrollView(
+                child: Column(children: [
+          SizedBox(height: height * 1.0 / 18.0),
+          Stack(alignment: Alignment.topLeft, children: [
+            Align(
                 alignment: Alignment.topLeft,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: IconButton(
-                      iconSize: width * 1.0 / 18.0,
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: ((context) {
-                          return OpenPage();
-                        })));
-                      },
-                      icon: const Icon(Icons.arrow_back)
-                    )
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Image.asset(
-                      "assets/dsdf1.png",
-                      height: height * 1.0 / 6.75,
-                      width: height * 1.0  / 6.75,
-                      alignment: Alignment.center
-                    )
-                  )
-                ]
+                child: IconButton(
+                    iconSize: width * 1.0 / 18.0,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: ((context) {
+                        return OpenPage();
+                      })));
+                    },
+                    icon: const Icon(Icons.arrow_back))),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset("assets/dsdf1.png",
+                    height: height * 1.0 / 6.75,
+                    width: height * 1.0 / 6.75,
+                    alignment: Alignment.center))
+          ]),
+          SizedBox(height: height * 1.0 / 18.0),
+          const Text(
+            "User Login",
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: height * 1.0 / 9.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
+            child: TextField(
+              controller: emailController,
+              textInputAction: TextInputAction.done,
+              cursorColor: Colors.white,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+                hintText: 'Enter your username',
               ),
-              SizedBox(height: height * 1.0 / 18.0),
-              const Text(
-                "User Login",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: height * 1.0 / 18.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
+            child: TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              obscuringCharacter: '*',
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter your password',
               ),
-              SizedBox(height: height * 1.0 / 9.0),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
-                child: TextField(
-                  controller: emailController,
-                  textInputAction: TextInputAction.done,
-                  cursorColor: Colors.white,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                    hintText: 'Enter your username',
-                  ),
-                ),
-              ),
-              SizedBox(height: height * 1.0 / 18.0),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
-                child: TextField(
-                  controller: passwordController,
-                  textInputAction: TextInputAction.done,
-                  obscureText: true,
-                  obscuringCharacter: '*',
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                  ),
-                ),
-              ),
-              SizedBox(height: height * 1.0 / 9.0),
-              TextButton(
-                style: TextButton.styleFrom(
-                  minimumSize: Size(width * 1.0 / 2.0, height * 1.0 / 13.5),
-                  foregroundColor: Colors.black,
-                  backgroundColor: Color.fromARGB(255, 200, 200, 200),
-                  textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                onPressed: signIn,
-              
-                  // Navigator.push(context, MaterialPageRoute(builder: ((context) {
-                  //   return HomePage();
-                  // })));
-                child: const Text('Login'),
-              ),
-            ]
-          )
-        )
-      )
-    );
+            ),
+          ),
+          SizedBox(height: height * 1.0 / 9.0),
+          TextButton(
+            style: TextButton.styleFrom(
+              minimumSize: Size(width * 1.0 / 2.0, height * 1.0 / 13.5),
+              foregroundColor: Colors.black,
+              backgroundColor: Color.fromARGB(255, 200, 200, 200),
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            onPressed: signIn,
+
+            // Navigator.push(context, MaterialPageRoute(builder: ((context) {
+            //   return HomePage();
+            // })));
+            child: const Text('Login'),
+          ),
+        ]))));
   }
 
   Future signIn() async {
