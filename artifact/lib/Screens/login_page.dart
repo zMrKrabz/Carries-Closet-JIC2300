77d2d5c8239a5dd84414cfeb9aeac45bd89f29f1
Steven Hua <stevenhua@gatech.edu>
2023-03-06 12:58:00 -0500

    for admin home screen

--- artifact/lib/Screens/login_page.dart
+++ artifact/lib/Screens/login_page.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import "package:artifact/main.dart";
 import "package:artifact/Screens/forgot_password.dart";
-import "package:artifact/home_page.dart";
+import 'package:artifact/admin_home_page.dart';
 import "package:artifact/main.dart";
 
 class LoginPage extends StatefulWidget {
@@ -27,102 +27,90 @@ class _LoginPageState extends State<LoginPage> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: height * 1.0 / 18.0),
-            Stack(
-              alignment: Alignment.topLeft,
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
+        debugShowCheckedModeBanner: false,
+        home: SingleChildScrollView(
+            child: Column(children: [
+          SizedBox(height: height * 1.0 / 18.0),
+          Stack(alignment: Alignment.topLeft, children: [
+            Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
                     iconSize: width * 1.0 / 18.0,
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: ((context) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: ((context) {
                         return OpenPage();
                       })));
                     },
-                    icon: const Icon(Icons.arrow_back)
-                  )
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Image.asset(
-                    "assets/dsdf1.png",
+                    icon: const Icon(Icons.arrow_back))),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset("assets/dsdf1.png",
                     height: height * 1.0 / 6.75,
-                    width: height * 1.0  / 6.75,
-                    alignment: Alignment.center
-                  )
-                )
-              ]
-            ),
-            SizedBox(height: height * 1.0 / 18.0),
-            const Text(
-              "User Login",
-              style: TextStyle(
+                    width: height * 1.0 / 6.75,
+                    alignment: Alignment.center))
+          ]),
+          SizedBox(height: height * 1.0 / 18.0),
+          const Text(
+            "User Login",
+            style: TextStyle(
                 color: Colors.black,
-                fontSize: 35, 
+                fontSize: 35,
                 fontWeight: FontWeight.bold,
-                decoration: TextDecoration.none
+                decoration: TextDecoration.none),
+          ),
+          SizedBox(height: height * 1.0 / 9.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
+            child: TextField(
+              controller: emailController,
+              textInputAction: TextInputAction.done,
+              cursorColor: Colors.white,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+                hintText: 'Enter your username',
               ),
             ),
-            SizedBox(height: height * 1.0 / 9.0),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
-              child: TextField(
-                controller: emailController,
-                textInputAction: TextInputAction.done,
-                cursorColor: Colors.white,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Enter your username',
-                ),
+          ),
+          SizedBox(height: height * 1.0 / 18.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
+            child: TextField(
+              controller: passwordController,
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              obscuringCharacter: '*',
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter your password',
               ),
             ),
-            SizedBox(height: height * 1.0 / 18.0),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
-              child: TextField(
-                controller: passwordController,
-                textInputAction: TextInputAction.done,
-                obscureText: true,
-                obscuringCharacter: '*',
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                ),
-              ),
-            ),
-            Padding(
+          ),
+          Padding(
               padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 40.0),
               child: TextButton(
-                child: Text('Forgot Password?'),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: ((context) {
+                  child: Text('Forgot Password?'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: ((context) {
                       return ForgotPassword();
-                  })));
-                }
-              )
+                    })));
+                  })),
+          SizedBox(height: height * 2.0 / 27.0),
+          TextButton(
+            style: TextButton.styleFrom(
+              minimumSize: Size(width * 1.0 / 2.0, height * 1.0 / 13.5),
+              foregroundColor: Colors.black,
+              backgroundColor: Color.fromARGB(255, 200, 200, 200),
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: height * 2.0 / 27.0),
-            TextButton(
-              style: TextButton.styleFrom(
-                minimumSize: Size(width * 1.0 / 2.0, height * 1.0 / 13.5),
-                foregroundColor: Colors.black,
-                backgroundColor: Color.fromARGB(255, 200, 200, 200),
-                textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              onPressed: signIn,
-              child: const Text('Login'),
-            ),
-          ]
-        )
-      )
-    );
+            onPressed: signIn,
+            child: const Text('Login'),
+          ),
+        ])));
   }
 
   Future signIn() async {
