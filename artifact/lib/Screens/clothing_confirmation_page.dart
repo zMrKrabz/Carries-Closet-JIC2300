6ce338076112e10bae7be56402b09e9d90dc30d6
Steven Hua <stevenhua@gatech.edu>
2023-03-08 13:01:05 -0500

    adding linkage between confirmation and drop downs

--- artifact/lib/Screens/clothing_confirmation_page.dart
+++ artifact/lib/Screens/clothing_confirmation_page.dart
@@ -5,6 +5,7 @@ import "package:artifact/main.dart";
 import 'package:artifact/Screens/clothing_page.dart';
 import "package:artifact/home_page.dart";
 import "package:artifact/main.dart";
+import "package:artifact/admin_home_page.dart";
 
 import 'package:http/http.dart' as http;
 
