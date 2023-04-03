e281e9e21c47c4f84314c23258e2169383913a3c
kbelwood3 <kbelwood3@gatech.edu>
2023-04-02 22:17:20 -0400

    updated UI for admin request page

--- artifact/lib/Screens/admin_request_page.dart
+++ artifact/lib/Screens/admin_request_page.dart
@@ -26,7 +26,7 @@ class _AdminRequestPageState extends State<AdminRequestPage> {
             body: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: Column(children: [
-                  SizedBox(height: height * 1.0 / 18.0),
+                  SizedBox(height: height * 1.0 / 13.5),
                   Stack(alignment: Alignment.topLeft, children: [
                     Align(
                         alignment: Alignment.topLeft,
@@ -41,10 +41,15 @@ class _AdminRequestPageState extends State<AdminRequestPage> {
                             icon: const Icon(Icons.arrow_back))),
                     Align(
                         alignment: Alignment.bottomCenter,
-                        child: Image.asset("assets/dsdf1.png",
-                            height: height * 1.0 / 6.75,
-                            width: height * 1.0 / 6.75,
-                            alignment: Alignment.center))
+                        child: Column(
+                          children: [
+                            SizedBox(height: height * 1.0 / 100.0),
+                            const Text(
+                              'Orders',
+                              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)
+                            )
+                          ],)
+                    )
                   ]),
                   RequestWidget(),
                 ]))));
@@ -63,7 +68,11 @@ class RequestWidget extends StatefulWidget {
 class _RequestWidgetState extends State<RequestWidget> {
   @override
   Widget build(BuildContext context) {
-    return Card(
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return DefaultTextStyle(
+      style: TextStyle(fontSize: 14),
       child: FutureBuilder<String>(
           future: parseRequests(),
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
@@ -73,42 +82,137 @@ class _RequestWidgetState extends State<RequestWidget> {
               return ListView.builder(
                   shrinkWrap: true,
                   itemCount: decode.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ListTile(
-                          title: Text("Request #1"),
-                          subtitle: Text("${decode[index]['date']}"),
-                          trailing: Icon(Icons.more_vert),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text("Item: ${decode[index]['item']}"),
-                            Text("Size: ${decode[index]['size']}")
-                          ],
-                        ),
-                        Text("Gender: ${decode[index]['gender']}"),
-                        Text("Address: ${decode[index]['address']}"),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              ElevatedButton(
-                                onPressed: null,
-                                child: const Text('Complete'),
+                  itemBuilder: (context, index) { 
+                    int reqNum = index + 1;
+
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width * 1.0 / 12.0),
+                      child: Card(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: height * 1.0 / 55.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Request #$reqNum',
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xFF2E2E2E)
+                                        ),
+                                        textAlign: TextAlign.left
+                                      ),
+                                      Text(
+                                        '${decode[index]['date']}',
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontStyle: FontStyle.italic,
+                                          color: Color(0xFF2E2E2E)
+                                        ),
+                                        textAlign: TextAlign.start,
+                                      )
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      SizedBox(width: width * 11.0 / 42.0)
+                                    ]
+                                  )
+                                ]
                               ),
-                              const SizedBox(width: 8),
-                              ElevatedButton(
-                                onPressed: null,
-                                child: Text('Deny'),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: width * 1.0 / 12.0,
+                                  vertical: height * 1.0 / 55.0
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Item #: ${decode[index]['item']}',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFF2E2E2E)
+                                      ),
+                                      textAlign: TextAlign.left
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: const [
+                                            Text(
+                                              'Size: ',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Color(0xFF808080)
+                                              ),
+                                              textAlign: TextAlign.left
+                                            ),
+                                            Text(
+                                              'Gender: ',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Color(0xFF808080),
+                                              ),
+                                              textAlign: TextAlign.left
+                                            )
+                                          ]
+                                        ),
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.end,
+                                          children: [
+                                            Text(
+                                              decode[index]['size'],
+                                              style: const TextStyle(
+                                                fontSize: 14,
+                                                color: Color(0xFF2E2E2E)
+                                              ),
+                                              textAlign: TextAlign.right,
+                                            ),
+                                            Text(
+                                              decode[index]['gender'],
+                                              style: const TextStyle(
+                                                fontSize: 14,
+                                                color: Color(0xFF2E2E2E)
+                                              ),
+                                              textAlign: TextAlign.right,
+                                            )
+                                          ],
+                                        )
+                                      ]
+                                    )
+                                  ]
+                                )
                               ),
-                              const SizedBox(width: 8),
-                            ])
-                      ],
+                              TextButton(
+                                onPressed: null,
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFFC4DBFE))
+                                ),
+                                child: Text(
+                                  'View Full Request',
+                                  style: TextStyle(
+                                    color: Color(0xFF2E2E2E)
+                                  ))
+                              )
+                            ]
+                          )
+                        ),
+                      )
                     );
-                  });
-            } else if (snapshot.hasError) {
+                  }
+              );
+            }
+            else if (snapshot.hasError) {
               children = <Widget>[
                 const Icon(
                   Icons.error_outline,
@@ -156,3 +260,131 @@ class _RequestWidgetState extends State<RequestWidget> {
     return response.body;
   }
 }
+
+// return Card(
+                    //   color: Color.fromRGBO(220,220,220,1.0),
+                    //   shape: const RoundedRectangleBorder(
+                    //     borderRadius: BorderRadius.only(
+                    //       topLeft: Radius.circular(16),
+                    //       bottomLeft: Radius.circular(16),
+                    //       topRight: Radius.circular(16), 
+                    //       bottomRight: Radius.circular(16))
+                    //   ),
+                    //   child: Column(
+                    //     mainAxisSize: MainAxisSize.min,
+                    //     children: [
+                    //     ListTile(
+                    //       title: Text(
+                    //         'Request #$reqNum',
+                    //         style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    //       subtitle: Text(
+                    //         '${decode[index]['date']}',
+                    //         style: const TextStyle(
+                    //           fontSize: 14, 
+                    //           fontStyle: FontStyle.italic, 
+                    //           color: Color(0xFF2E2E2E)
+                    //         )
+                    //       ),
+                    //       // trailing: Icon(Icons.clean_hands_rounded)
+                    //       // trailing: Icon(Icons.local_laundry_service_rounded)
+                    //     ),
+                    //     Padding(
+                    //       padding: EdgeInsets.symmetric(
+                    //         horizontal: width * 1.0 / 12.0,
+                    //         vertical: height * 1.0 / 55.0),
+                    //       child: Column(
+                    //       crossAxisAlignment: CrossAxisAlignment.start,
+                    //       children: [
+                    //         const Text(
+                    //           'Item #1',
+                    //           style: TextStyle(
+                    //             fontSize: 16,
+                    //             fontWeight: FontWeight.bold,
+                    //             color: Color(0xFF2E2E2E)
+                    //           )
+                    //         ),
+                    //         Row(
+                    //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //           children: [
+                    //             Column(
+                    //               crossAxisAlignment: CrossAxisAlignment.start,
+                    //               children: const [
+                    //                 Text(
+                    //                   'Size:',
+                    //                   textAlign: TextAlign.left,
+                    //                   style: TextStyle(fontSize: 14, color: Color(0xFF808080))
+                    //                 ),
+                    //                 Text(
+                    //                   'Gender:',
+                    //                   textAlign: TextAlign.left,
+                    //                   style: TextStyle(fontSize: 14, color: Color(0xFF808080))
+                    //                 )
+                    //               ]
+                    //             )
+                    //           ]
+                    //         )
+                    //       ]
+                    //     )
+                    //   )
+                        // Row(
+                        //   mainAxisAlignment: MainAxisAlignment.start,
+                        //   children: [
+                        //     SizedBox(width: width * 1.0 / 24.0),
+                        //     Text(
+                        //       'Item #1: ${decode[index]['item']}',
+                        //       style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+                        //     )
+                        //   ]
+                        // ),
+                        // Row(
+                        //   mainAxisAlignment: MainAxisAlignment.start,
+                        //   children: [
+                        //     SizedBox(width: width * 1.0 / 24.0),
+                        //     Text(
+                        //       'Size:   ${decode[index]['size']}',
+                        //     )
+                        //   ]
+                        // ),
+                        // Row(
+                        //   children: [
+                        //     SizedBox(height: height * 1.0 / 40.0)
+                        //   ]
+                        // )
+                        // Row(
+                        //   mainAxisAlignment: MainAxisAlignment.start,
+                        //   children: [
+                        //     Text()
+                        //   ]
+                        // )
+                      //   ListTile(
+                      //     title: Text("Request #$reqNum"),
+                      //     subtitle: Text("${decode[index]['date']}"),
+                      //     trailing: Icon(Icons.more_vert),
+                      //   ),
+                      //   Row(
+                      //     mainAxisAlignment: MainAxisAlignment.start,
+                      //     children: [
+                      //       Text("Item: ${decode[index]['item']}"),
+                      //       Text("Size: ${decode[index]['size']}")
+                      //     ],
+                      //   ),
+                      //   Text("Gender: ${decode[index]['gender']}"),
+                      //   Text("Address: ${decode[index]['address']}"),
+                      //   Row(
+                      //       mainAxisAlignment: MainAxisAlignment.center,
+                      //       children: <Widget>[
+                      //         ElevatedButton(
+                      //           onPressed: null,
+                      //           child: const Text('Complete'),
+                      //         ),
+                      //         const SizedBox(width: 8),
+                      //         ElevatedButton(
+                      //           onPressed: null,
+                      //           child: Text('Deny'),
+                      //         ),
+                      //         const SizedBox(width: 8),
+                      //       ])
+                      // ],
+                  //   )
+                  //   );
+                  // });
