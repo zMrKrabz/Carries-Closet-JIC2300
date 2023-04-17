61e962d97858c7c6f64b4275dafe6fd02216114f
ephraimkwon <epjkwon.11@gmail.com>
2023-04-17 00:13:47 -0400

    add profile page known information

--- artifact/lib/main.dart
+++ artifact/lib/main.dart
@@ -62,7 +62,7 @@ class MainPage extends StatelessWidget {
           },
         ),
       );
-  Future<bool> checkPermissions(BuildContext context, String uid) async {
+  Future checkPermissions(BuildContext context, String uid) async {
     bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
     var url = isIOS
         ? Uri.parse('http://127.0.0.1:8080/users?id=$uid')
